8beb4f29c660445ed72150bead86ec435d356607
aorri2 <aorri96@naver.com>
2023-05-21 14:55:26 +0900

    feat : 프로젝트 초기 설정

---
+++ src/main/java/com/example/hjw_echo/HjwEchoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hjw_echo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjwEchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HjwEchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hjw_echo/HjwEchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hjw_echo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjwEchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
